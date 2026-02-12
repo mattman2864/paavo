9db70722433ea6a4aa91c01fb54eadb8fa8dad0a
Matt Stein <mattman2864@gmail.com>
2026-02-12 16:04:37 -0500

    Remove build directory from version control

--- build/CMakeFiles/paavo.dir/src/Player.cpp.o.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/paavo.dir/src/Player.cpp.o: \
- /home/matt/Programming/projects/games/paavo/src/Player.cpp \
- /usr/include/stdc-predef.h \
- /home/matt/Programming/projects/games/paavo/src/Player.h \
- /home/matt/Programming/projects/games/paavo/src/Sprite.h \
- /home/matt/libs/raylib/install/include/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h \
- /home/matt/Programming/projects/games/paavo/src/Body.h \
- /home/matt/Programming/projects/games/paavo/src/Input.h
